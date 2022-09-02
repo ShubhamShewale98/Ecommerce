ad1ef703fd68bb9aa6411989529767e4f6e0b71f
ShubhamShewale98 <shubhamshewale498@gmail.com>
2022-09-02 15:37:18 +0530

    basic setup

---
+++ mini_ecommerce_app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E Commerce</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
